817ddbbb7ce401a2872a328fef26c1d15cf8a576
Avanishyadav9035 <yadavavnish683@gmail.com>
2025-07-26 11:45:05 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
-    <div>
+    <div style={{padding:'50px'}}>
         {/* Do not remove the main div */}
+        <Tooltip text="This is a tooltip">
+         <h1 style={{cursor:"pointer"}}>Hover over me</h1>
+        </Tooltip>
+        <br/> <br/>
+        <Tooltip text="This is another tooltip">
+         <p style={{cursor:"pointer"}}>Hover over me to see another tooltip</p>
+        </Tooltip>
     </div>
   )
 }
